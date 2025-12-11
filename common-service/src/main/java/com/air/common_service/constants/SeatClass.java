4738f95e7e8ae14e947ad2af3bb35dd7af7bf7f7
khoihuynhanh <khoihuynh.git@gmail.com>
2025-12-11 10:27:00 +0700

    update seat class enum

--- common-service/src/main/java/com/air/common_service/constants/SeatClass.java
+++ common-service/src/main/java/com/air/common_service/constants/SeatClass.java
@@ -1,6 +1,19 @@
 package com.air.common_service.constants;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum SeatClass {
     ECONOMY,
-    BUSINESS
+    BUSINESS;
+
+    @JsonCreator
+    public static SeatClass fromString(String key) {
+        return key == null ? null : SeatClass.valueOf(key.toUpperCase());
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name();
+    }
 }
